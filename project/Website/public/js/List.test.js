bfdd6283a4b0664b184289fee9f0ec0ac599d273
chasewalker26 <chase.walker@live.com>
2021-02-20 17:56:20 -0500

    Creates test for List.updateDatabase()

--- project/Website/public/js/List.test.js
+++ project/Website/public/js/List.test.js
@@ -1,4 +1,18 @@
-testing("getProduct returns the target product from the database when given a barcode", () =>
+require('./main');
+const buildList = require('./List');
+
+test("updateDatabase is functional",  async () =>
 {
-  let list1 = new List()
+  var list = buildList('ListID');
+  var data;
+
+  await list.updateDatabase(list.products);
+  var dbProductsAfterSave = await pullFromFirebase('ProductList/ListID/');
+
+  if (JSON.stringify(list.products) == JSON.stringify(dbProductsAfterSave))
+    data = true;
+  else
+    data = false;
+
+  expect(data).toBe(true);
 })
