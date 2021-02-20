9491f1e585e209319824bf50ede5558164f8fb75
chasewalker26 <chase.walker@live.com>
2021-02-20 17:50:57 -0500

    Adds Node.js for testing firebase functions with  jest

--- project/Website/public/js/main.js
+++ project/Website/public/js/main.js
@@ -1,36 +1,28 @@
-function testTest()
+// ALWAYS set false when not testing
+var testing = true;
+
+// [REQUIRED FOR TESTING]
+// Enables export of testing functions and initializes firebase with prewritten
+// Node.js firebase functions from Google's documentation
+if (testing == true)
 {
-    return true;
+    // module.exports = getList;
+
+    // Initializes firebase using Node.js for tests
+    firebase = require("firebase/app");
+    require("firebase/database");
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyAgxD_jRTQ-5PDtrWjsNd5KazN5LNYBU-U",
+        authDomain: "cop4331-project.firebaseapp.com",
+        databaseURL: "https://cop4331-project-default-rtdb.firebaseio.com",
+        projectId: "cop4331-project",
+        storageBucket: "cop4331-project.appspot.com",
+        messagingSenderId: "620770546311",
+        appId: "1:620770546311:web:9e858d72e45d8d89cd9e22",
+        measurementId: "G-NRS2BW86XT"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 }
-
-function getList(){
-    var listItems;
-
-    firebase.database().ref("/ProductList/ListID/").on("value", function(snapshot){
-        var lists = snapshot.val();
-        listItems = Object.keys(lists);
-        console.log(lists);
-    });
-
-    setTimeout(function() {
-        getProducts(listItems)
-    }, 1000);
-
-    return listItems;
-}
-
-function getProducts(listItems)
-{
-    console.log(listItems);
-    for (var i = 0; i < listItems.length; i++)
-    {
-        firebase.database().ref("/ProductList/ListID/" + listItems[i]).on("value", function(snapshot){
-            var lists = snapshot.val();
-            console.log(lists);
-        });
-    }
-}
-
-
-
-module.exports = testTest;
