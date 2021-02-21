a86c931d0abd5e86b2e5594e711d69a18a35194e
chasewalker26 <chase.walker@live.com>
2021-02-21 12:27:31 -0500

    Deletes and replaces Jest tests for main and List

--- project/Website/public/js/main.js
+++ project/Website/public/js/main.js
@@ -1,41 +1,16 @@
 // ALWAYS set false when not testing
-var testing = true;
-
-// [REQUIRED FOR TESTING]
-// Enables export of testing functions and initializes firebase with prewritten
-// Node.js firebase functions from Google's documentation
-if (testing == true)
-{
-    // module.exports = getList;
-
-    // Initializes firebase using Node.js for tests
-    firebase = require("firebase/app");
-    require("firebase/database");
-
-    var firebaseConfig = {
-        apiKey: "AIzaSyAgxD_jRTQ-5PDtrWjsNd5KazN5LNYBU-U",
-        authDomain: "cop4331-project.firebaseapp.com",
-        databaseURL: "https://cop4331-project-default-rtdb.firebaseio.com",
-        projectId: "cop4331-project",
-        storageBucket: "cop4331-project.appspot.com",
-        messagingSenderId: "620770546311",
-        appId: "1:620770546311:web:9e858d72e45d8d89cd9e22",
-        measurementId: "G-NRS2BW86XT"
-    };
-
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-}
+var isTesting = true;
 
 // function that enables asynchronous fethcing of database data
 pullFromFirebase = (datapath) => 
 {
     return new Promise((resolve) => 
     {
-        firebase.database().ref(datapath).once('value').then(function(snapshot) 
+        firebase.database().ref(datapath).on('value', function(snapshot) 
         {
             const products = snapshot.val();
-            resolve(products);
+            if (products != undefined)
+                resolve(products);
         });
     })
 }
--- project/Website/public/js/main.test.js
+++
@@ -1,6 +0,0 @@
-const testTest = require("./main");
-
-test("Returns correct products from list in database", () => {
-    const data = testTest();
-    expect(data).toBe(true);
-});
