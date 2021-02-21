d5289ca14c79458059298599f66a97fa567e1c71
Christopher Gray <chrisgrayak@gmail.com>
2021-02-21 12:31:42 -0500

    changed parameters for product constuctor

--- project/Website/public/js/Product.js
+++ project/Website/public/js/Product.js
@@ -10,13 +10,20 @@ class Product{
         this.warningCount = product.warningCount;
         this.warningDay = product.warningDay;
     }
+
     getProduct(barcode){
-        
+
     }
 }
 
 
-const product1 = new Product(123,2,4,"bannas","10:32",4,3);
-const product2 = new Product(1234,42,44,"apples","10:35",8,2);
+const product1 = new Product("product 0", {
+    "count" : 5,
+    "idealCount": 1,
+    "name" : "name",
+    "timeScanned": 0,
+    "warningCount": -1,
+    "warningDay":  -1 
+});
 
 //console.log(product1);
