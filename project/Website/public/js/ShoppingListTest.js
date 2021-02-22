f63b3e9a76a55bd12c62f9da46ec8585b5b8cc11
chasewalker26 <chase.walker@live.com>
2021-02-21 19:03:28 -0500

    Consolidates tests related to shoppingList.html

--- project/Website/public/js/ShoppingListTest.js
+++ project/Website/public/js/ShoppingListTest.js
@@ -1,3 +1,6 @@
+// 
+// This test file contains all tests related to shoppingList.html
+// 
 if (isTesting == true)
 {
     window.onload = function()
@@ -8,9 +11,127 @@ if (isTesting == true)
 
 function runTests()
 {
+    ListTest();
+    ProductTest();
+    getProductsTest();
+    updateDatabaseTest();
     formatListTest();
 }
 
+// WRITE LIST TEST
+// in Test.js
+function ListTest()
+{
+
+}
+
+// in Product.js
+function ProductTest(){
+    var data;
+    var product0 = new Product("Barcode0", {
+        "count" : 5,
+        "idealCount": 1,
+        "name" : "name",
+        "timeScanned": 0,
+        "warningCount": -1,
+        "warningDay":  -1 
+    });
+
+    var product1 =
+    {
+        "barcode" : "Barcode0",
+        "count" : 5,
+        "idealCount": 1,
+        "name" : "name",
+        "timeScanned": 0,
+        "warningCount": -1,
+        "warningDay":  -1
+    }
+    if (JSON.stringify(product1) == JSON.stringify(product0))
+        data = true;
+    else
+        data = false;
+
+    console.assert(data == true, "ProductTest FAILED");
+}
+
+// in Test.js
+async function getProductsTest()
+{
+    let list = new List("ListID");
+    var data;
+    var products = [
+        {
+            "barcode" : "Barcode0",
+            "count" : 5,
+            "idealCount": 1,
+            "name" : "name",
+            "timeScanned": 0,
+            "warningCount": -1,
+            "warningDay":  -1
+        },
+        {
+            "barcode" : "Barcode1",
+            "count" : 2,
+            "idealCount": 1,
+            "name" : "name",
+            "timeScanned": 0,
+            "warningCount": -1,
+            "warningDay":  -1
+        }
+    ]
+
+    await list.getProducts();
+
+    if (JSON.stringify(list.products) == JSON.stringify(products))
+        data = true;
+    else
+        data = false;
+
+    console.assert(data == true, "getProductsTest FAILED");
+}
+
+// in Test.js
+async function updateDatabaseTest()
+{
+    var list = new List('ListID');
+    var data;
+
+    list.products =
+    {
+        "Barcode0" :
+        {
+            "count" : 5,
+            "idealCount": 1,
+            "name" : "name",
+            "timeScanned": 0,
+            "warningCount": -1,
+            "warningDay":  -1
+        },
+        "Barcode1" :
+        {
+            "count" : 2,
+            "idealCount": 1,
+            "name" : "name",
+            "timeScanned": 0,
+            "warningCount": -1,
+            "warningDay":  -1
+        }
+    }
+
+    await list.updateDatabase(list.products);
+
+    var dbProducts = await pullFromFirebase("ProductList/ListID/");
+
+    if (JSON.stringify(list.products) == JSON.stringify(dbProducts))
+        data = true;
+    else
+        data = false;
+
+    console.assert(data == true, "updateDatabaseTest FAILED");
+}
+
+// in ShoppingList.js
 async function formatListTest()
 {
     let shoppingList = new ShoppingList("ListID");
@@ -29,8 +150,5 @@ async function formatListTest()
     for (var i = 0; i < siteShoppingListElements.length; i++)
         actualElements += siteShoppingListElements[i].outerHTML;
 
-    console.log(actualElements);
-    console.log(expectedElements);
-
-    console.assert(expectedElements == actualElements, "formatListTest() FAILED");
+    console.assert(expectedElements == actualElements, "ShoppingList.formatListTest() FAILED");
 }
