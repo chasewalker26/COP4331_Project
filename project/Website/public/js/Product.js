17cce0445c4a9daf5ff94aec6e7bf6b89fce73e2
chasewalker26 <chase.walker@live.com>
2021-02-23 20:42:39 -0500

    Removes commented code

--- project/Website/public/js/Product.js
+++ project/Website/public/js/Product.js
@@ -8,46 +8,4 @@ class Product{
         this.warningCount = product.warningCount;
         this.warningDay = product.warningDay;
     }
-
-    /*
-    getProduct(barcode){
-
-        //converts 
-        var product = 
-        {  
-            "barcode" : "Barcode0",
-            "count" : 5,
-            "idealCount": 1,
-            "name" : "name",
-            "timeScanned": 0,
-            "warningCount": -1,
-            "warningDay":  -1
-        }
-
-        // to 
-        var product = 
-        {
-            "Barcode0" :
-            {
-                "count" : 5,
-                "idealCount": 1,
-                "name" : "name",
-                "timeScanned": 0,
-                "warningCount": -1,
-                "warningDay":  -1
-            }
-        }
-    }*/
 }
-
-
-const product1 = new Product("product 0", {
-    "count" : 5,
-    "idealCount": 1,
-    "name" : "name",
-    "timeScanned": 0,
-    "warningCount": -1,
-    "warningDay":  -1 
-});
-
-//console.log(product1);
