f4ebae51a2807faed91e2d20269429ae7b468037
chasewalker26 <chase.walker@live.com>
2021-02-25 20:34:09 -0500

    updates getProducts error handling

--- project/Website/public/js/List.js
+++ project/Website/public/js/List.js
@@ -8,7 +8,10 @@ class List
   
   async getProducts()
   {
-    var dbProducts = await pullFromFirebase("ProductList/" + this.listID + "/");
+    var dbProducts = await pullFromFirebase("ProductList/" + this.listID + "/").catch((error) => {
+      console.log(error);
+      alert("There is no list associated with this account. Have you scanned any items?");
+    });
     var barcodes = Object.keys(dbProducts);
 
     for (var i = 0; i < barcodes.length; i++)
