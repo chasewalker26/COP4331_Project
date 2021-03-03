8515a02b7566cbd96c1b847016d0344768f65aa3
Christopher Gray <chrisgrayak@gmail.com>
2021-03-02 19:34:23 -0500

    created contact class

---
+++ project/Website/public/js/Contact.js
@@ -0,0 +1,11 @@
+//event listener from submit
+class contact {
+    constructor(userEmail,message,devEmail) {
+        this.userEmail = userEmail;
+        this.message = message;
+        this.devEmail = devEmail;     
+    }
+    
+
+}
+
