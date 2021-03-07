2f1296f067afad44fd6aa4fd6b750d24da1bb8d2
aramsahai09 <54642757+aramsahai09@users.noreply.github.com>
2021-03-07 13:29:08 -0500

    Adding popup for items not recognized in inventory

--- project/Website/public/js/Inventory.js
+++ project/Website/public/js/Inventory.js
@@ -16,9 +16,24 @@ class Inventory extends List
       var name = products[i].name;
       var count = products[i].count;
 
-      html += '<li class="listProduct" id="' + barcode + '" name="inventoryItem" onClick="inventory.editItem()"><span class="material-icons mx-2">edit</span>' + name + ': ' + count + '</li>';
+      if (name)
+      {
+        html += '<li class="listProduct" id="' + barcode + '" name="inventoryItem" onClick="inventory.editItem()"><span class="material-icons mx-2">edit</span>' + name + ': ' + count + '</li>';
+      }
+      else
+      {
+        html += '<li class="notFound"><div class="notFound" data-toggle = "modal" data-target = #myModal>' + barcode + ' </div> </li>';
+      }
     }
 
     document.getElementById("inventory").innerHTML = html;
+  }
+
+  nameItem()
+  {
+    var name; 
+    var count; 
+    this.products.name = name;
+    this.products.count = count;
   }
 }
