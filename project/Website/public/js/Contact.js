996c8659d7b78fed7aab4d82b242815c74c75ae5
Christopher Gray <chrisgrayak@gmail.com>
2021-03-08 19:00:53 -0500

    added event listener, and sent email

--- project/Website/public/js/Contact.js
+++ project/Website/public/js/Contact.js
@@ -1,11 +1,28 @@
-//event listener from submit
+// event listener from submit
 class Contact {
     constructor(userEmail,message,devEmail) {
         this.userEmail = userEmail;
         this.message = message;
         this.devEmail = devEmail;     
     }
-    
+}
+sendMail();
 
+function sendMail() {
+    (function () {
+        // https://dashboard.emailjs.com/admin/integration
+        emailjs.init('user_qBNYciotCBLgcKwufO8iz');
+    })();
+
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('service_7igsl3v', 'template_q3vcveq', this)
+            .then(function () {
+                console.log('SUCCESS!');
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
+    });
 }
 
